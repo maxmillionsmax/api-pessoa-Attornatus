2de5852a90ee691e137aef2f70e0a84fe07f502d
maxmillionsmax <maximilianocaetano@gmail.com>
2023-01-11 00:01:09 -0300

    criação da api de pessoa

---
+++ src/main/java/com/api/pessoa/ApiPessoaApplication.java
@@ -0,0 +1,13 @@
+package com.api.pessoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPessoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPessoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/pessoa/ApiPessoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.pessoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPessoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
