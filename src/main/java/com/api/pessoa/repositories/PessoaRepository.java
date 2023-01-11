dd69418903dd28515bdd592f947c030c26b5cb9e
maxmillionsmax <maximilianocaetano@gmail.com>
2023-01-11 00:59:54 -0300

    repositories criados

---
+++ src/main/java/com/api/pessoa/repositories/PessoaRepository.java
@@ -0,0 +1,11 @@
+package com.api.pessoa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.api.pessoa.domain.Pessoa;
+
+@Repository
+public interface PessoaRepository extends JpaRepository<Pessoa, Integer>{
+
+}
