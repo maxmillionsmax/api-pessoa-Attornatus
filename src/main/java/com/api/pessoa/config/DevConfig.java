15f9fda2f8619abea10f1678f63e33ed0fc0d8bd
maxmillionsmax <maximilianocaetano@gmail.com>
2023-01-11 15:26:39 -0300

    Criação de perfil de desenvolvimento

---
+++ src/main/java/com/api/pessoa/config/DevConfig.java
@@ -0,0 +1,28 @@
+package com.api.pessoa.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.api.pessoa.service.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instanciaBaseDeDados() {
+		if (strategy.equals("create")) {
+			this.dbService.instanciaBaseDeDados();
+		}
+		return false;
+	}
+}
